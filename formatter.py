7c0c95a8038a4f4b5a55b9530a2ce20adde3f8d8
NOP-nop-NOP <fiertki@gmail.com>
2025-09-15 01:06:18 +0800

    support more formats

--- formatter.py
+++ formatter.py
@@ -8,14 +8,14 @@ def formatter(output_path, transcription, audio_file):
 		name, ext = os.path.splitext(f)
 		if ext:
 			extensions.add(ext)
+	file_name_without_path = os.path.basename(audio_file)
+	file_name = os.path.splitext(file_name_without_path)[0]
 	if '.json' in extensions:
-		file_name_without_path = os.path.basename(audio_file)
-		file_name = os.path.splitext(file_name_without_path)[0]
 		word_to_json(transcription, os.path.join(output_path, f"{file_name}.json"))
-	# if '.srt' in extensions:
-	# 	file_name_without_path = os.path.basename(audio_file)
-	# 	file_name = os.path.splitext(file_name_without_path)[0]
-	# 	word_to_srt(transcription, os.path.join(output_path, f"{file_name}.srt"))
+	if '.txt' in extensions:
+		word_to_txt(transcription, os.path.join(output_path, f"{file_name}.txt"))
+	if '.srt' in extensions:
+		word_to_srt(transcription, os.path.join(output_path, f"{file_name}.srt"))
 	
 def format_srt_time(seconds: float) -> str:
 	frac, whole = math.modf(seconds)
@@ -85,6 +85,72 @@ def save_json(json_data, file_name):
 	with open(file_name, 'a+', encoding='utf-8-sig') as f:
 		json.dump(json_data, f, indent=4, ensure_ascii=False)
 	print(f"json saved to {file_name}")
+
+def word_to_txt(transcription, file_path):
+	txt_data = []
+	text_buffer = ''
+	speaker_buffer = ''
+	for word in transcription.words:
+		if word.type != 'word':
+			if text_buffer:
+				txt_data.append(f"({speaker_buffer}){text_buffer}")
+			if word.type == 'audio_event':
+				txt_data.append(f"{word.text}")
+			text_buffer = ''
+		else:
+			if word.speaker_id != speaker_buffer:
+				if text_buffer:
+					txt_data.append(f"({speaker_buffer}){text_buffer}")
+					text_buffer = ''
+				speaker_buffer = word.speaker_id
+			text_buffer += word.text
+	if text_buffer:
+		txt_data.append(f"({speaker_buffer}){text_buffer}")
+	save_txt(txt_data, file_path)
+
+def save_txt(txt_data, file_name):
+	with open(file_name, 'a+', encoding='utf-8-sig') as f:
+		f.write('\n'.join(txt_data))
+	print(f"txt saved to {file_name}")
+
+def word_to_srt(transcription, file_path):
+	srt_data = []
+	srt_index = 1
+	text_buffer = ''
+	speaker_buffer = ''
+	start_time = 0
+	end_time = 0
+	for word in transcription.words:
+		end_time = word.end
+		if start_time == 0:
+			start_time = word.start
+			speaker_buffer = word.speaker_id
+		if word.type != 'word':
+			if text_buffer:
+				srt_data.append(f"{srt_index}\n{format_srt_time(start_time)} --> {format_srt_time(end_time)}\n<{speaker_buffer}>{text_buffer}\n")
+				srt_index += 1
+			if word.type == 'audio_event':
+				srt_data.append(f"{srt_index}\n{format_srt_time(word.start)} --> {format_srt_time(end_time)}\n{word.text}\n")
+				srt_index += 1
+			text_buffer = ''
+			start_time == 0
+		else:
+			if word.speaker_id != speaker_buffer:
+				if text_buffer:
+					srt_data.append(f"{srt_index}\n{format_srt_time(start_time)} --> {format_srt_time(end_time)}\n<{speaker_buffer}>{text_buffer}\n")
+					srt_index += 1
+				text_buffer = ''
+				start_time = word.start
+				speaker_buffer = word.speaker_id
+			text_buffer += word.text		
+	if text_buffer:
+		srt_data.append(f"{srt_index}\n{format_srt_time(start_time)} --> {format_srt_time(end_time)}\n<{speaker_buffer}>{text_buffer}\n")
+	save_srt(srt_data, file_path)
+	
+def save_srt(srt_data, file_name):
+	with open(file_name, 'a+', encoding='utf-8-sig') as f:
+		f.write('\n'.join(srt_data))
+	print(f"srt saved to {file_name}")
 
 if __name__ == "__main__":
 	from main import saveload_transcription
